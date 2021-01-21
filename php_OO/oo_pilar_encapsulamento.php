7182d4277c57c63cfd7cd115da70201891545665
kevenescovedo <49500205+kevenescovedo@users.noreply.github.com>
2021-01-21 10:21:49 -0300

    Add files via upload

--- php_OO/oo_pilar_encapsulamento.php
+++ php_OO/oo_pilar_encapsulamento.php
@@ -12,11 +12,69 @@ class Pai {
     public function getNome() {
     return $this->nome;
     }
+    public function setNome($value) {
+       if(strlen($value) >= 0){
+        $this->nome = $value;
+       }
+    }
+    public function getSobrenome() {
+        return $this->sobrenome;
+        }
+        public function setSobrenome($value) {
+           if(strlen($value) >= 0){
+            $this->sobrenome = $value;
+           }
+        }
+    function __get($atributo) {
+        return $this->$atributo;
+
+    }
+    function __set($atributo, $value) {
+        return $this->$atributo = $value;
+        
+    } 
+    private function executarMania() {
+        echo "Assobiar";
+    }
+    protected function responder() {
+        echo "oi";
+    }
+    public function executarAcao() {
+       $x = rand(1,10);
+       if($x >=1 and $x <= 8 ) {
+        $this->executarMania();
+       }
+       else {
+        $this->responder();
+       }
+    }    
+}
+class Filho extends Pai {
+    public function getAtributo($atributo) {
+        return $this->$atributo;
+    }
+    public function setAtributo($atributo,$valor) {
+         $this->$atributo = $valor;
+    }
+
 }
 $pai = new Pai();
 $pai->humor = "Muito Feliz";
 echo $pai->getNome();
 echo "</br>";
 echo $pai->humor;
+
+
+// os metodos magicos __get and set __ nos permite acessar dados protects e privates na aplicacação;
+echo "</br>";
+echo $pai->nome;
+echo $pai->executarAcao();
+$filho = new Filho();
+
+print_r($filho);
+$filho->setAtributo("nome","leandro");
+print_r($filho);
+//pega os metodos de uma classe
+print_r(get_class_methods($filho));
 
 ?>
